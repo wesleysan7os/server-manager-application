96969c74011f8d02c1fc98ed4cbde9e84bb00425
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2023-02-01 21:47:50 -0300

    add the application service

---
+++ src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
@@ -0,0 +1,33 @@
+package io.getarrays.server.service.implementation;
+
+import io.getarrays.server.model.Server;
+import io.getarrays.server.service.ServerService;
+
+import java.util.Collection;
+
+public class ServerServiceImpl implements ServerService {
+    @Override
+    public Server create(Server server) {
+        return null;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        return null;
+    }
+
+    @Override
+    public Server get(Long id) {
+        return null;
+    }
+
+    @Override
+    public Server update(Server server) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+}
