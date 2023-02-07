43030774e7ac93b4978cdb7b04eeb54f60d8f288
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2023-02-07 13:33:50 -0300

    mapping HTTP response

--- src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
@@ -9,10 +9,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
@@ -64,6 +66,7 @@ public class ServerServiceImpl implements ServerService {
     }
 
     private String setServerImageUrl() {
-        return null;
+        String [] imageNames = { "server1.png", "server2.png", "server3.png", "server4.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imageNames[new Random().nextInt(4)]).toUriString();
     }
 }
