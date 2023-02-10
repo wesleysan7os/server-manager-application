3fc1889df3af64406880c8df4fc8bde00fe0ac17
igorpaiva <igor.araujo@ee.ufcg.edu.br>
2023-02-10 15:24:51 -0300

    remove unused imports

--- src/main/java/io/getarrays/server/ServerApplication.java
+++ src/main/java/io/getarrays/server/ServerApplication.java
@@ -6,7 +6,6 @@ import io.getarrays.server.repository.ServerRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
